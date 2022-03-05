ecb60985ed23b13f40ea96ed95448798f964a6ea
RenuSuresh <rganvir500@gmail.com>
2022-03-05 21:49:04 +0530

    first step

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // constructor
+    // store candidate
+    // read candidate
+    string public candidate;
+
+    constructor() public {
+        candidate = "Candidate1";
+    }
+}
